86cb229aa7cdfcddfbb86b05ebf9980471ea4de8
hoangkhang21 <80323053+hoangkhang21@users.noreply.github.com>
2022-10-27 17:16:47 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+
+	<title>Quản lý nhân viên</title>
+
+
+</head>
+<body>
+	<div class="main">
+		<!-- Menu: sử dụng navbar -->
+		<?php include 'navbar.php'; ?>
+		
+		
+		<div class="card mt-3">
+			<div class="card-header">Nhom 20</div>
+			
+			<div class="card-body">
+			<div class="row">
+			  <div class="col-lg-4"></div>
+			  <div class="col-lg-4">
+				<p class="card-text"><h1>Đồ Án Quản Lý Đề Án Phần Mềm</h1></p>
+			  </div>
+			  <div class="col-lg-4"></div>
+
+			</div>
+			</div>
+			<div class="row">
+			  <div class="col-lg-1">
+			  </div>
+			  <div class="col-lg-2">
+				<img src="img/MN.jpg" alt="Avatar" style="width:250px">
+					<h4><b>Nguyễn Ra Băng</b></h4>
+					<h5>DTH195127 - DH20TH1</h5>
+			  </div>
+			  <div class="col-lg-2">
+				 <img src="img/MN.jpg" alt="Avatar" style="width:250px">
+					<h4><b>Nguyễn Minh Ngọc</b></h4>
+					<h5>DTH195160 - DH20TH1</h5>
+				
+			  </div>
+			  <div class="col-lg-2">
+				<img src="img/MN.jpg" alt="Avatar" style="width:250px">
+					<h4><b>Thái Hoàng Khang</b></h4>
+					<h5>DTH195144 - DH20TH1</h5>
+			  </div>
+			  <div class="col-lg-2">
+				<img src="img/MN.jpg" alt="Avatar" style="width:250px">
+					<h4><b>Trịnh Hoàng Đông Nghi</b></h4>
+					<h5>DTH195313 - DH20TH1</h5>
+			  </div>
+			  <div class="col-lg-2">
+				<img src="img/MN.jpg" alt="Avatar" style="width:250px">
+					<h4><b>Bùi Huỳnh Kim Cương</b></h4>
+					<h5>DTH195253 - DH20TH1</h5>
+			  </div>
+			  <div class="col-lg-1">
+			  </div>
+			</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+		<!-- Footer: tự code -->
+		<?php include 'footer.php'; ?>
+	</div>
+	
+</body>
+</html>
+
+		
---
+++ footer.php
@@ -0,0 +1,2 @@
+<hr class="mb-0">
+<footer>Bản quyền https://www.facebook.com/profile.php?id=100009559632544; 2022 bởi DH20TH1 .</footer>
