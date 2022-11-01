7a78ad3fccc4f004a73c405266a7aa6d3553242e
hoangkhang21 <80323053+hoangkhang21@users.noreply.github.com>
2022-11-01 15:37:12 +0700

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Website Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  </style>
+</head>
+<body>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1>KIEN AN HOTEL</h1>
+  <p>18.Ung Van Khiem, Dong Xuyen Ward, Long Xuyen City, An Giang Province</p> 
+</div>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h1>CONTACT</h1>
+	   <img src="img/letan.jpg" class="card-img-top" alt="ava" style="width: 380px;">
+      <h5>HOTLINE: 0941747540- ZALO:098848653</h5>
+	  <h5>EMAIL:kienan@gmail.com</h5>
+      <h3>TIỆN ÍCH KHÁCH SẠN</h3>
+	  <img src="img/nhahang.jpg" class="card-img-top" alt="ava" style="width: 380px;">
+		 <p>Nghỉ ngơi sau một chuyến đi xa tại phòng nghỉ Khách sạn, 
+			 Quý khách hãy tìm đến với dư vị ẩm thực phong phú của Nhà hàng Kiến An. 
+			 Tại đây với Nhà hàng Á và Nhà hàng Âu,
+			 Quý khách có thể lựa chọn cho mình nhiều phong cách ẩm thực khác nhau
+			 và các món đặc sản An Giang đến những món ăn nổi tiếng Việt Nam </p>
+		<img src="img/beboi.jpg" class="card-img-top" alt="ava" style="width: 380px;">
+		<p>Bể bơi Hoàng Sơn hiện diện với những làn nước xanh như một nét tô điểm duyên dáng cho Khách sạn. 
+			Sau những giờ nghỉ ngơi tại phòng nghỉ Khách sạn và thưởng thức các món ăn độc đáo, 
+			Quý khách sẽ thư giãn với làn nước trong vắt, mát lạnh của Bể bơi. </p>
+      <ul class="nav nav-pills nav-stacked">
+       
+      </ul>
+      <hr class="hidden-sm hidden-md hidden-lg">
+    </div>
+    <div class="col-sm-8">
+      <h1>INTRODUCE</h1>
+	  <img src="img/khachsan.jpg" style="width: 750px;">
+      <h5>Thành lập: ngày 15, tháng 7, năm 2017</h5>
+      <p>Khách sạn Kiến An tọa lạc gần quảng trường Trưng Nữ Vương  - Nằm giữa trung tâm kinh tế và văn hóa của Thành Phố Long Xuyên, 
+			Khách sạn Kiến An  mong muốn là cánh cửa đầu tiên ở Long Xuyên chào đón Quý khách trong nước và Quốc tế về với An Giang, 
+			miền đất sở hữu 2 loại hình du lịch đặc trưng là du lịch sinh thái và du lịch văn hóa tâm linh.  </p>
+	<p>Với chất lượng đạt chuẩn Quốc tế 4 sao, 
+		Khách sạn Kiến An không chỉ đáp ứng hoàn hảo nhu cầu nghỉ ngơi của Quý khách mà còn mang đến những giây phút thư giãn giải trí tuyệt vời và để lại dư vị ấn tượng trong văn hóa ẩm thực nơi đây. 
+		Khách sạn là sự kết hợp hài hòa của phong cách mang màu sắc sang trọng, hiện đại và cổ kính bao gồm cả quần thể Nhà hàng, Bể Bơi,
+		sân Tennis và các khu vui chơi giải trí khác được quy tụ trong một khuôn viên rộng lớn gần 15.000 m2 .</p>
+      <h2>ROOM</h2>
+	 <img src="img/phong.jpg" class="card-img-top" alt="ava" style="width: 240px;">
+	 <img src="img/phongdoi.jpg" class="card-img-top" alt="ava" style="width: 230px;">
+	 <img src="img/phong3.jpg" class="card-img-top" alt="ava" style="width: 210px;">
+      <p>Khách Kiến An sạn được thiết kế bao gồm 12 tầng và 214 phòng các loại với nội thất cao cấp và sang trọng.
+		Tất cả các phòng nghỉ của Khách sạn đều có cửa sổ lớn nhìn ra không gian bên ngoài với xa xa là khung cảnh non nước hữu tình đầy thơ mộng và cảnh đẹp lung linh của Thành phố Long Xuyên khi màn đêm buông xuống để mặc những ánh đèn đua nhau thắp lên. Hơn thế nữa,  
+		Khách sạn Kiến An còn lựa chọn cách thiết kế độc đáo, thuận tiện cho phòng VIP, 
+		Family Suite và Excutive Suite trong đó phòng khách và phòng nghỉ được tách biệt với diện tích phòng lên đến 72m2</p>
+    </div>
+  </div>
+</div>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>Footer</p>
+</div>
+
+</body>
+</html>
